a18e0028051306945affaf59861241aa61ffd520
Shamaru Primera <sprimera@moveapps.cl>
2025-10-26 08:41:14 -0400

    first commit

---
+++ web API/Controllers/root.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Security.Cryptography.X509Certificates;
+using System.Numerics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.OpenApi.Any;
+
+namespace web_API.Controllers
+{
+    [ApiController]
+    [Route("/shamaru")]
+    public class Root : ControllerBase
+    {
+        [HttpGet(Name = "shamaru 2")]
+        public string Get()
+        {
+            
+            Console.WriteLine(HttpContext.Request.Headers);
+
+            foreach (KeyValuePair<string, StringValues> a in HttpContext.Request.Headers)
+            {
+                Console.WriteLine(a);
+            }
+            return "shamaru";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+
+
+
+    }
+}

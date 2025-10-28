53fb6e79f3037beacde051b54e238007192b7a17
Shamaru Primera <sprimera@moveapps.cl>
2025-10-28 07:40:39 -0400

    Actualiza el flujo de trabajo para mejorar la implementación de IIS y ajusta la salida de publicación

--- web API/Controllers/WeatherForecastController.cs
+++ web API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace web_API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "shamaru"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
